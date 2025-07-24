32a81184be940b66cee10b926f374604c83022f9
PrincessAzraiel <azraelhascome@gmail.com>
2025-07-24 14:13:34 +0200

    weeee

--- src/pages/NewProgram.tsx
+++ src/pages/NewProgram.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Button } from '@/components/ui/button';
 import ReactDOM from 'react-dom';
-import { link } from 'fs';
+// import { link } from 'fs';
 
 const characters = [
   {
